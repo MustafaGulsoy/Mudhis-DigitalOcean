5d502904b1c8370a8c3d77a36750542f21b748b6
Mustafa Gülsoy <mustafagulsoy05@gmail.com>
2024-04-18 20:11:11 +0300

    For tiktok 8

--- blog/urls.py
+++ blog/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path, include
-from .views import Index, Featured, DeleteArticleView, DetailArticleView, LikeArticle, Tiktok, can_use
+from .views import Index, Featured, DeleteArticleView, DetailArticleView, LikeArticle, Tiktok
 
 urlpatterns = [
     path('', Index.as_view(), name='index'),
