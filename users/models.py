c33a0d85162b418f01df4e7319cb2061e73c90ec
Mustafa Gülsoy <mustafagulsoy05@gmail.com>
2024-04-24 20:58:18 +0300

    Satrting models

--- users/models.py
+++ users/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
+
 # Create your models here.
+class Child(models.Model):
+    name = models.CharField(max_length=255)
+
+
+class Member(models.Model):
+    name = models.CharField(max_length=255)
+    bird_date = models.DateField(auto_now_add=True)
+    sex = models.BooleanField()
+
+    date = models.DateField(auto_now_add=True)
+
+    featured = models.BooleanField(default=False)
+    likes = models.ManyToManyField(Child, related_name='likes')
+
+
