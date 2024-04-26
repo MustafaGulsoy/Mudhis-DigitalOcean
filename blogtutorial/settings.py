06438e11bd1ccb7bdd8e62567ceba5da5cfed875
Mustafa Gülsoy <mustafagulsoy05@gmail.com>
2024-04-27 02:50:24 +0300

    JWT Done

--- blogtutorial/settings.py
+++ blogtutorial/settings.py
@@ -46,8 +46,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'corsheaders',
     'rest_framework',
-    'corsheaders'
+
 
 ]
 
