0d7b45d6ca7cee0434f5ce0d4d59b8831b2c5731
Mustafa Gülsoy <mustafagulsoy05@gmail.com>
2024-04-30 16:52:34 +0300

    JWT Done

--- blog/migrations/0001_initial.py
+++ blog/migrations/0001_initial.py
@@ -13,16 +13,16 @@ class Migration(migrations.Migration):
         migrations.swappable_dependency(settings.AUTH_USER_MODEL),
     ]
 
-    operations = [
-        migrations.CreateModel(
-            name='Article',
-            fields=[
-                ('id', models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
-                ('title', models.CharField(max_length=255)),
-                ('content', models.TextField()),
-                ('date', models.DateField(auto_now_add=True)),
-                ('published', models.BooleanField()),
-                ('author', models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, to=settings.AUTH_USER_MODEL)),
-            ],
-        ),
-    ]
+    # operations = [
+    #     migrations.CreateModel(
+    #         name='Article',
+    #         fields=[
+    #             ('id', models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
+    #             ('title', models.CharField(max_length=255)),
+    #             ('content', models.TextField()),
+    #             ('date', models.DateField(auto_now_add=True)),
+    #             ('published', models.BooleanField()),
+    #             ('author', models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, to=settings.AUTH_USER_MODEL)),
+    #         ],
+    #     ),
+    # ]
